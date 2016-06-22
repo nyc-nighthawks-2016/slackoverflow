cd5634dca706322e9269a5ff7a629061da3fb7c2
Jonathan Andrew Philippou & Erica Rufrano & Jeffrey Ng & Seth Daniel Lawson Goldman <pair+conturbo.ericatalyst.jeffreymng.sethdaniel89@devbootcamp.com>
2016-06-22 19:46:49 -0400

    Adds a funky cool new login drop down box that if logged in properly, redirects the user back to the home page.

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('#login-trigger').on('mouseover', function(e){
+  $('#login-trigger').on('click', function(e){
     e.preventDefault;
     // debugger
     $(this).next('#login-content').slideToggle();
@@ -7,14 +7,24 @@ $(document).ready(function(){
 
     if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
       else $(this).find('span').html('&#x25BC;')
-      $("input[name=Email]").focus();
-    // $.ajax({
-    //   url: $(this).attr('href');
-    //   method: 'POST'
-    //   data: $('#inputs').serialize();
-    // })
-    // .done(function() {
 
-    // })
+     $("input[name=email]").focus();
+
+    $('#drop-login').on('submit', function(e) {
+        e.preventDefault;
+        var login = $(e.target).serialize();
+        var thing = $.ajax({
+          url: $(this).attr('action'),
+          method: $(this).attr('method'),
+          data: login
+        });
+        thing.done(function(response) {
+          window.location.href = response;
+        });
+        thing.fail(function(response) {
+          $('#login-content').html(response);
+        });
     });
+  });
+
 });
