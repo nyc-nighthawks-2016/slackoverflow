27955c574e0f8aa2ac257cb372029b0efa149b7f
Jonathan Andrew Philippou & Erica Rufrano & Jeffrey Ng & Seth Daniel Lawson Goldman <pair+conturbo.ericatalyst.jeffreymng.sethdaniel89@devbootcamp.com>
2016-06-22 17:28:11 -0400

    Beginning to work on ajax, jquery.

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  $('#login-trigger').click(function(e){
+    e.preventDefault;
+    $(this).next('#login-content').slideToggle();
+    $(this).toggleClass('active');
 
-})
+    if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
+      else $(this).find('span').html('&#x25BC;')
+      debugger
+    $.ajax({
+      url: $(this).attr('href');
+      method: 'POST'
+    })
+    .done(function() {
+
+    })
+    })
+});
