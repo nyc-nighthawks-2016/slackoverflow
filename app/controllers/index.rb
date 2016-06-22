7fa5e3c0dfd2ca2f5203c2909c5fff6ea683638d
Jonathan Andrew Philippou & Erica Rufrano & Jeffrey Ng & Seth Daniel Lawson Goldman <pair+conturbo.ericatalyst.jeffreymng.sethdaniel89@devbootcamp.com>
2016-06-22 16:07:05 -0400

    Completes user login, register, and logout functionality, and allows difference links to display in the navbar based on if logged in.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,44 @@
 get '/' do
   erb :index
 end
+
+#User register
+
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users/new' do
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/'
+    else
+      @errors = user.errors.full_messages
+      erb :'users/new'
+    end
+
+end
+
+#User login
+
+get '/users/login' do
+  erb :'users/login'
+end
+
+post '/users/login' do
+  @user = User.find_by(email: params[:email])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = @user.errors.full_messages
+  end
+end
+
+#User Logout
+
+get '/users/logout' do
+  session.delete(:user_id)
+  redirect '/'
+end
