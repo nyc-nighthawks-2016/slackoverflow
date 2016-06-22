d4df91c92146a7106d2532bde4c5426ee21be2a4
Jonathan Andrew Philippou & Erica Rufrano & Jeffrey Ng & Seth Daniel Lawson Goldman <pair+conturbo.ericatalyst.jeffreymng.sethdaniel89@devbootcamp.com>
2016-06-22 18:11:25 -0400

    Working on some ajax functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,21 +6,21 @@ end
 
 post '/sessions/login' do
   @user = User.find_by(email: params[:email])
-  if @user
-    status 200
-    if request.xhr?
-    if @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect '/'
+  # status 200
+  # if request.xhr?
+    if @user
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect '/'
+      else
+        # binding.pry
+        @errors = @user.errors.full_messages
+        erb :'sessions/login'
+      end
     else
-      # binding.pry
-      @errors = @user.errors.full_messages
+      @errors = ['User does not exist!!!']
       erb :'sessions/login'
     end
-  else
-    @errors = ['User does not exist!!!']
-    erb :'sessions/login'
-  end
 end
 
 #User Logout
--- public/js/application.js
+++ public/js/application.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
-  $('#login-trigger').click(function(e){
+  $('#login-trigger').on('mouseover', function(e){
     e.preventDefault;
+    // debugger
     $(this).next('#login-content').slideToggle();
     $(this).toggleClass('active');
 
     if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
       else $(this).find('span').html('&#x25BC;')
-      debugger
-    $.ajax({
-      url: $(this).attr('href');
-      method: 'POST'
-    })
-    .done(function() {
+      $("input[name=Email]").focus();
+    // $.ajax({
+    //   url: $(this).attr('href');
+    //   method: 'POST'
+    //   data: $('#inputs').serialize();
+    // })
+    // .done(function() {
 
-    })
-    })
+    // })
+    });
 });
